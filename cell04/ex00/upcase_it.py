7c2c974f514b29b71fe529007202c64c231620ed
HtooHtooAung046 <htoohtooaung2006@gmail.com>
2025-06-25 02:49:30 +0700

    .

--- cell04/ex00/upcase_it.py
+++ cell04/ex00/upcase_it.py
@@ -1,4 +1,4 @@
-#!user/bin/env python3
+#!/usr/bin/env python3
 banana = input("Give me a word: ")
 upper_banana = banana.upper()
 print(upper_banana)
---
+++ cell04/ex01/age.py
@@ -0,0 +1,12 @@
+#!/usr/biin/env python3
+y_age = int(input("Please tell me your age: "))
+if y_age <= 1:
+    print(f'You are currently {y_age} year old')
+else:
+    print (f'You are currently {y_age} years old')
+y10= y_age + 10
+y20= y_age + 20
+y30= y_age + 30
+print(f"In 10 years, you will be {y10} years old")
+print(f"In 20 years, you will be {y20} years old")
+print(f"In 30 years, you will be {y30} years old")
