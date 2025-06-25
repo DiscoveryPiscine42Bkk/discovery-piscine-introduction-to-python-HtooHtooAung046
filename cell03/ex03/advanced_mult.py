e60aad161c7ee048cfdf5813f6485c00537f550d
HtooHtooAung046 <htoohtooaung2006@gmail.com>
2025-06-25 11:34:51 +0700

    .

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-i = 0
-while i <= 10:
-    print(f'Table de {i}:',end=" ")
-    j = 0
-    while j <= 10:
-        print(i * j,end= " " if j<10 else "\n")
-        j += 1
-    i += 1
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 1
+    while i <= 10:
+        print(f"Table de {i}:", end=" ")
+        j = 0
+        result = 0
+        while j <= 10:
+            print(result, end=" ")
+            result += i  # add i instead of doing i * j
+            j += 1
+        print()
+        i += 1
