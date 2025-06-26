ca1e4a822a651da64f0b2752103b94e709867c2e
HtooHtooAung046 <htoohtooaung2006@gmail.com>
2025-06-26 15:52:14 +0700

    .

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -7,6 +7,5 @@ def downcase_all(x):
 if len(sys.argv) > 1:
     for i in sys.argv[1:]:
         print(downcase_all(i))
-
 else:
     print("none")
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,7 @@
+#!/sur/bin/env python3
+def add_one(x):
+    x += 1 
+pmt = 10
+print('Before : ',pmt)
+add_one(pmt)
+print('After : ',pmt)
