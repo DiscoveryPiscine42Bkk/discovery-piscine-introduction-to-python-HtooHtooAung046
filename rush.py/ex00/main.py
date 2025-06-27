1c2a2681b8fb2c7f226dc94733f322c9949b0fa0
HtooHtooAung046 <htoohtooaung2006@gmail.com>
2025-06-27 12:10:36 +0700

    .

---
+++ rush.py/ex00/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from checkmate import checkmate
+board = [
+        [".", "R", ".", "."],
+        [".", "K", ".", "."],
+        [".", ".", ".", "."],
+        [".", ".", ".", "."]
+]
+checkmate(board)
